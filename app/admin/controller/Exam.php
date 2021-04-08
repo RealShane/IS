4b74931698c322739b671f985ac7e2ecc6f4f6c3
lichenying <2949794718@qq.com>
2021-04-08 17:51:04 +0800

    更新

--- app/admin/controller/Exam.php
+++ app/admin/controller/Exam.php
@@ -6,11 +6,22 @@ namespace app\admin\controller;
 
 use app\BaseController;
 use app\common\business\admin\Exam as examBusiness;
+use app\common\validate\admin\User as Validate;
+
 class Exam extends BaseController
 {
     public function commitPaper(){
-        $file = $this -> request -> file('file');
-        $errCode = (new examBusiness()) -> commitPaper($file);
+        $data['class_id'] = $this -> request -> param('class_id');
+        $data['file'] = $this -> request -> file('file');
+        $data['beginTime'] = $this -> request -> param('beginTime');
+        $data['closeTime'] = $this -> request -> param('closeTime');
+        try {
+            validate(Validate::class) -> scene('commitPaper') -> check($data);
+        }catch (\Exception $exception){
+            return $this -> fail($exception -> getMessage());
+        }
+        (new examBusiness()) -> commitPaper($data);
+        return $this -> success("添加试卷成功！");
     }
 
 }
---
+++ app/common/validate/admin/Exam.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *
+ * @description: 人生五十年，与天地长久相较，如梦又似幻
+ *
+ * @author: Shane
+ *
+ * @time: 2020/9/30 14:37
+ */
+
+
+namespace app\common\validate\admin;
+
+
+use think\Validate;
+
+class Exam extends Validate
+{
+
+    protected $rule = [
+        'class_id|班级id' => 'require',
+        'file|文件' => 'require',
+        'beginTime|开始时间' => 'require',
+        'closeTime|结束时间' => 'require',
+    ];
+
+    protected $scene = [
+        'commitPaper' => ['class_id', 'file', 'beginTime', 'closeTime'],
+    ];
+
+}
