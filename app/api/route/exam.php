3f6679bff5a97100314661ee18b4d48f9b6d8972
lichenying <2949794718@qq.com>
2021-04-09 19:48:23 +0800

    更新

---
+++ app/api/route/exam.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *
+ * @description: 永远爱颍
+ *
+ * @author: Shane
+ *
+ * @time: 2021/4/9 15:29
+ */
+use think\facade\Route;
+use \app\api\middleware\IsLogin;
+
+
+Route::group(function () {
+    Route::rule('View/Exam/Papers/index', '/admin/View/examPapersManageView', 'GET');
+    Route::rule('View/Exam/Papers/add', '/admin/View/examPapersAddView', 'GET');
+    Route::rule('View/Exam/Papers/edit', '/admin/View/examPapersEditView', 'GET');
+});
+
+Route::group('Exam', function () {
+    Route::rule('showPaper', '/api/Exam/showPaper', 'POST');
+
+}) -> middleware(IsLogin::class);
+
+//----------------------------------------------------------------------------------
+/*
+                          _ooOoo_
+                         o8888888o
+                         88" . "88
+                         (| -_- |)
+                         O\  =  /O
+                      ____/`---'\____
+                    .'  \\|     |//  `.
+                   /  \\|||  :  |||//  \
+                  /  _||||| -:- |||||-  \
+                  |   | \\\  -  /// |   |
+                  | \_|  ''\---/''  |   |
+                  \  .-\__  `-`  ___/-. /
+                ___`. .'  /--.--\  `. . __
+             ."" '<  `.___\_<|>_/___.'  >'"".
+            | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+            \  \ `-.   \_ __\ /__ _/   .-` /  /
+       ======`-.____`-.___\_____/___.-`____.-'======
+                          `=---='
+       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                    佛祖保佑       永无BUG
+       */
