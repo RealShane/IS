80bed5ca864bacaedf98cccd6e2de13ca2acc0a2
Shane <1109571639@qq.com>
2021-04-09 21:48:08 +0800

    更新

---
+++ app/index/controller/Index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * @description: 永远爱颍
+ *
+ * @author: Shane
+ *
+ * @time: 2021/4/9 21:46
+ */
+
+
+namespace app\index\controller;
+
+
+use app\BaseController;
+
+class Index extends BaseController
+{
+
+    public function index(){
+        return $this -> success("ssssindex");
+    }
+
+}
