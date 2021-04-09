8154b012b40491256d0df1aeba0b879b0df157bc
lichenying <2949794718@qq.com>
2021-04-09 12:05:56 +0800

    更新

--- app/common/model/api/ExamPapers.php
+++ app/common/model/api/ExamPapers.php
@@ -16,7 +16,7 @@ class ExamPapers extends Model
     ];
 
     public function selectTitle($title, $num){
-        return $this -> where('title', 'LIKE', '%' . $title . '%') -> paginate($num);
+        return self::with('classes') -> where('title', 'LIKE', '%' . $title . '%') -> paginate($num);
     }
 
     public function findAll($num){
