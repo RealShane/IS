c6044580f31571159c538b8f5f926d125ab2b62b
lichenying <2949794718@qq.com>
2021-04-10 09:00:10 +0800

    更新

--- app/common/business/api/Exam.php
+++ app/common/business/api/Exam.php
@@ -37,6 +37,7 @@ class Exam
     public function showPaperTitle($uid){
         $classId = $this -> userClassModel -> findByUid($uid);
         $papers = $this -> examPapersModel -> findByClassId($classId['class_id']);
+        echo  $papers['title'];exit();
         foreach ($papers as $paper){
             $data[] = [
                 'title' => $paper['title']
