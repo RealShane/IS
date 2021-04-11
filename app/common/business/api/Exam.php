cec43117d9aed1bf058bf461a8c8537e216284c3
lichenying <2949794718@qq.com>
2021-04-11 19:14:14 +0800

    更新

--- app/common/business/api/Exam.php
+++ app/common/business/api/Exam.php
@@ -85,8 +85,7 @@ class Exam
         if ($time < $paper['close_time']['begin_time']) {
             throw new Exception("未到答题时间");
         }
-        echo "我应该不显示";
-        exit();
+        echo json_encode($answer);exit();
         if ($time > $paper['close_time']['close_time']) {
             if ((int)$user['status']) {
                 $data['answer'] = $user['answer'];
