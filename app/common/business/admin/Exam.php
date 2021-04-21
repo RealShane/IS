f9ccc8ff89fd0a63a82d1faa7e76e8af0adf35eb
lichenying <2949794718@qq.com>
2021-04-21 20:07:40 +0800

    更新

--- app/common/business/admin/Exam.php
+++ app/common/business/admin/Exam.php
@@ -78,9 +78,10 @@ class Exam
         $res = $this -> examPapersModel -> findAll($num) -> each(function ($res){
             foreach ($res['class_id'] as $item){
                 $temp = $this -> classesModel -> findById($item);
-                echo json_encode($temp);exit();
-                $res['classes']['name'] = $temp['name'];
+                echo json_encode($temp);
+//                $res['classes']['name'] = $temp['name'];
             }
+            exit();
         });
         echo json_encode($res);
         exit();
