19a9ca2b755aecc56071f7c1a06b3c8c4974669c
lichenying <2949794718@qq.com>
2021-05-06 16:17:43 +0800

    q

--- app/common/business/admin/Synthesize.php
+++ app/common/business/admin/Synthesize.php
@@ -69,10 +69,9 @@ class Synthesize
             for ($i = 0; $i < $cout; $i++){
                 $temp['rater' . $i] = $tem[$i];
             }
-            x
             $temp['avgScore'] = $sum;
             $temp['sumScore'] = $avgScore;
-            echo json_encode($tem[1]);exit();
+            echo json_encode($temp);exit();
             $res[] = $temp;
 
             $user[] = $userName;
