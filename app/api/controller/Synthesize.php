04b5e99d65ac33eebcc17c2462297c1a6a7756c8
lichenying <2949794718@qq.com>
2021-05-07 11:28:34 +0800

    q

--- app/api/controller/Synthesize.php
+++ app/api/controller/Synthesize.php
@@ -109,10 +109,11 @@ class Synthesize extends BaseController
             validate(UploadValidate::class) -> checkRule(['file' => $file], 'checkFile');
         } catch (\Exception $exception) {
             $message = $exception -> getMessage();
-            echo json_encode($message);exit();
+
             if ($exception -> getCode() == 4){
                 $message = '未上传文件！';
             }
+            echo json_encode($exception -> getCode());exit();
             return $this -> fail($message);
         }
         $saveName = $this -> upload -> upload($user, $file, 'synthesize_poor', 'synthesize/poor/');
