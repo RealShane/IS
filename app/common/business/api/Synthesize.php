8f920c852c4009f90245f3e953bfdd4b30589821
lichenying <2949794718@qq.com>
2021-05-08 09:05:57 +0800

    q

--- app/common/business/api/Synthesize.php
+++ app/common/business/api/Synthesize.php
@@ -249,7 +249,6 @@ class Synthesize
         if (empty($isExist)){
             throw new Exception("该学生未报名贫困生！");
         }
-        echo json_encode($isExist['supporting_document']);exit();
         return download($isExist['supporting_document'], 'test.txt', true);
     }
 
