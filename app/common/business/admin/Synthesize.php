f0b979b83cb038afe64d105f85ac74c4f43eaed7
lichenying <2949794718@qq.com>
2021-05-08 17:41:38 +0800

    更新

--- app/common/business/admin/Synthesize.php
+++ app/common/business/admin/Synthesize.php
@@ -126,9 +126,9 @@ class Synthesize
             if (empty($sign) || empty($key) || empty($department) || empty($user)){
                 continue;
             }
-            echo json_encode(implode(",", $sign['confirm_reason']));exit();
+            echo json_encode(implode(",", $sign['confirm_reason']));
             $sign['confirm_reason'] = implode(",", $sign['confirm_reason']);
-
+            echo json_encode($sign['confirm_reason']);exit();
             $data[] = $this -> packPoorSignData($department, $user, $sign, $class);
         }
         echo json_encode($data);exit();
