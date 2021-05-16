c99401afd4877098dabc492382c4990753dceeee
Shane <1109571639@qq.com>
2021-05-16 14:09:10 +0800

    更新

---
+++ app/admin/route/user.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ *
+ * @description: 人生五十年，与天地长久相较，如梦又似幻
+ *
+ * @author: Shane
+ *
+ * @time: 2020/9/23 14:22
+ */
+
+use think\facade\Route;
+use \app\admin\middleware\Auth;
+use \app\admin\middleware\IsLogin;
+
+Route::group('View/User', function () {
+    Route::rule('index', '/admin/View/userView', 'GET');
+    Route::rule('edit', '/admin/View/userEditView', 'GET');
+});
+
+Route::group('User', function () {
+    Route::rule('getAllClass', '/admin/Synthesize/getAllClass', 'POST');
+    Route::rule('getTargetClass', '/admin/Synthesize/getTargetClass', 'POST');
+    Route::rule('getAllConfig', '/admin/Synthesize/getAllConfig', 'POST');
+    Route::rule('setConfig', '/admin/Synthesize/setConfig', 'POST');
+}) -> middleware(IsLogin::class) -> middleware(Auth::class);
+//----------------------------------------------------------------------------------
+/*
+                          _ooOoo_
+                         o8888888o
+                         88" . "88
+                         (| -_- |)
+                         O\  =  /O
+                      ____/`---'\____
+                    .'  \\|     |//  `.
+                   /  \\|||  :  |||//  \
+                  /  _||||| -:- |||||-  \
+                  |   | \\\  -  /// |   |
+                  | \_|  ''\---/''  |   |
+                  \  .-\__  `-`  ___/-. /
+                ___`. .'  /--.--\  `. . __
+             ."" '<  `.___\_<|>_/___.'  >'"".
+            | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+            \  \ `-.   \_ __\ /__ _/   .-` /  /
+       ======`-.____`-.___\_____/___.-`____.-'======
+                          `=---='
+       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                    佛祖保佑       永无BUG
+       */
